2a0bbd58dac0f78682800b990fcd94183e969cf4
kendreshivam817-max <kendreshivam817@gmail.com>
2026-08-01 20:20:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("giftBtn").addEventListener("click", function () {
+    alert("❤️ Happy Girlfriend's Day, Piu! ❤️\n\nThis is only the beginning...\nA much bigger surprise is coming! 🥰");
+});
